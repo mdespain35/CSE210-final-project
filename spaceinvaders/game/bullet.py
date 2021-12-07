d26986105106bbb66d863932e1e6caef7d7e29ec
Percevel <aidenjpenman@gmail.com>
2021-12-07 16:04:54 -0700

    Final Bullet

--- spaceinvaders/game/bullet.py
+++ spaceinvaders/game/bullet.py
@@ -4,23 +4,17 @@ import constants
 # The almighty bullet, destroyer of all, aliens, barriers, or ships. None survive the wrath of the bullet
 
 class Bullet(arcade.Sprite):
-    def __init__(self, x, y, direction):
+    def __init__(self, player:arcade.Sprite, direction):
         filepath = constants.BULLET_SPRITE
         super().__init__(filepath, constants.SCALING)
         # TODO: Set dy value of velocity equal to the bullet speed constant multiplied by direction
-        self.dy = constants.BULLET_SPEED * direction
+        self.change_y = constants.BULLET_SPEED * direction
         # Set point values equal to the passed x and y respectively
-        self.bx = x
-        self.by = y
+        self.center_x = player.center_x
+        self.center_y = player.center_y
 
 
-    def draw_self(self):
-        # TODO: Implement initial drawing of bullet using arcade package
-        pass
-
-    def advance(self):
-        # TODO: Add dy value of velocity to y value of center, check if bullet goes off screen.
-        self.velocity = (0 , self.dy)
+    def outOfBounds(self):
         # If bullet goes off screen. Kill it.
         if(self.top > 600 or self.bottom < 0):
             self.remove_from_sprite_lists()
