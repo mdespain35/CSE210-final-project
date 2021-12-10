af5478e6118f6796dd47ffac3bc130a9438e91bf
Jeremy Jackson <jectproxisdev@gmail.com>
2021-12-09 20:45:24 -0700

    Cleaned up the Alien class

--- spaceinvaders/game/alien.py
+++ spaceinvaders/game/alien.py
@@ -10,21 +10,15 @@ class Alien(arcade.Sprite):
         # TODO: Set velocity equal to alien speed constant, set center values equal to x, y values passed
         self.chance_of_firing = random.randint(1, 3)
 
-        self.center_x = x
-        self.center_y = y 
-        self.enemy_list = arcade.SpriteList()
-
-        self.enemy = arcade.Sprite(filename, constants.SCALING)
-        self.enemy.velocity = (constants.ALIEN_SPEED, 0) 
+        self.center_x = constants.SCREEN_WIDTH / 2
+        self.center_y = 600
 
     def advance(self):
         # TODO: Advance aliens along screen, once they hit the edge of screen, move them down and reverse dx value from velocity
-        for alien in self.enemy_list:
-            if alien.right == 0 or alien.left == 800:
-                for enemy in self.enemy_list:
+        if self.right == 0 or self.left == 800:
 
-                    enemy.center_y = enemy.center_y - 10
-                    enemy.reverse(self.enemy.change_x)
+                self.center_y = self.center_y - 10
+                self.reverse(self.change_x)
 
     def fire_bullet(self):
         # TODO: Have each alien have a small chance of firing a bullet every 3 seconds
