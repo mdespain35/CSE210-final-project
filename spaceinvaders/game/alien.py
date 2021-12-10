166f885e5576d68a9c511104f1bea3b4cd2bc70d
Jeremy Jackson <jectproxisdev@gmail.com>
2021-12-09 22:27:16 -0700

    Alien now fires bullets

--- spaceinvaders/game/alien.py
+++ spaceinvaders/game/alien.py
@@ -1,6 +1,7 @@
 import arcade
 from game import constants
 import random
+from game.bullet import Bullet
 # The space invaders themselves
 
 class Alien(arcade.Sprite):
@@ -8,10 +9,10 @@ class Alien(arcade.Sprite):
         filename = constants.ALIEN_SPRITE
         super().__init__(filename, constants.ALIEN_SCALING)
         # TODO: Set velocity equal to alien speed constant, set center values equal to x, y values passed
-        self.chance_of_firing = random.randint(1, 3)
         self.change_x = constants.ALIEN_SPEED
         self.center_x = constants.SCREEN_WIDTH / 2
         self.center_y = constants.SCREEN_HEIGHT
+        self.bullet_fire = arcade.load_sound(constants.SHIP_SOUND)
 
     def advance(self):
         # TODO: Advance aliens along screen, once they hit the edge of screen, move them down and reverse dx value from velocity
@@ -20,7 +21,11 @@ class Alien(arcade.Sprite):
             self.center_y = self.center_y - 10
             self.change_x = self.change_x * -1
 
-    def fire_bullet(self):
-        # TODO: Have each alien have a small chance of firing a bullet every 3 seconds
-        # Wait to implement this until bullets have been implemented. 
-        pass
+    # def fire_bullet(self,):
+    #     # TODO: Have each alien have a small chance of firing a bullet every 3 seconds
+    #     # Wait to implement this until bullets have been implemented. 
+    #     random_chance_of_firing = random.randint(1,10)
+
+    #     if random_chance_of_firing == 10:
+    #         arcade.play_sound(self.bullet_fire)
+    #         self.bullet
